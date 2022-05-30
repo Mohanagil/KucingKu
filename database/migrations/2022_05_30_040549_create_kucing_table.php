1fdb7b4a39a0ecebbc0140900463d7534cd7a2ab
Muhammad Agil Maulana <mohanagil82@gmail.com>
2022-05-30 12:04:55 +0700

    tambah database dan page admin

---
+++ database/migrations/2022_05_30_040549_create_kucing_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kucing', function (Blueprint $table) {
+            $table->id();
+            $table->string('ras');
+            $table->string('umur');
+            $table->string('berat');
+            $table->string('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kucing');
+    }
+};
