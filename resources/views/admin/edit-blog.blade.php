8307eb3696fde30e3500ea70092dfcfb75f979d1
Muhammad Agil Maulana <mohanagil82@gmail.com>
2022-06-02 14:25:19 +0700

    crud blog

--- resources/views/admin/edit-blog.blade.php
+++ resources/views/admin/edit-blog.blade.php
@@ -1,23 +1,24 @@
 @extends('admin.app')
 @section('content')
 <div class="container-fluid px-4">
-    <h1 class="mt-4">Tambah Artikel Blog</h1>
+    <h1 class="mt-4">Edit Artikel Blog</h1>
     <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Add Blog Article</li>
+        <li class="breadcrumb-item active">Edit Blog Article</li>
     </ol>
-    <form action="{{route('blog.store')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{route('blog.update',$blog->id)}}" method="POST" enctype="multipart/form-data">
         @csrf
+        @method('PUT')
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Judul Artikel</label>
-          <input type="text" class="form-control" id="judul" name="judul" aria-describedby="emailHelp">
+          <input type="text" class="form-control" id="judul" name="judul" aria-describedby="emailHelp" value="{{$blog->judul}}" required="">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Penulis</label>
-            <input type="text" class="form-control" id="penulis" name="penulis" aria-describedby="emailHelp">
+            <input type="text" class="form-control" id="penulis" name="penulis" aria-describedby="emailHelp" value="{{$blog->penulis}}" required="">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Isi Artikel</label>
-            <textarea class="form-control" id="deskrispi" name="deskripsi" rows="5" style="overflow-y: scroll;"></textarea>
+            <textarea class="form-control" id="deskrispi" name="deskripsi" rows="5" style="overflow-y: scroll;"required="">{{$blog->deskripsi}}</textarea>
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Foto</label>
